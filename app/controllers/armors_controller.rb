685eec4bfbc1989cec6eec9cc94bcd8c670f5f29
tacooyaki <p_mercredi@yahoo.ca>
2024-02-27 14:27:55 -0600

    Added search functionality via search bar and drop down for armors.

--- app/controllers/armors_controller.rb
+++ app/controllers/armors_controller.rb
@@ -1,11 +1,24 @@
 class ArmorsController < ApplicationController
   def index
-    @armors = Armor.page(params[:page]).per(25) # Adjust if need be
+    @types = ['all', 'head', 'chest', 'gloves', 'waist', 'legs']
+
+    # If you want to filter by the armor type
+    if params[:type].present? && params[:type] != 'all'
+      filtered_armors = Armor.where("type = ?", params[:type])
+    else
+      filtered_armors = Armor.all
+    end
+
+    # For search functionality of all armors
+    if params[:search].present?
+      search_term = params[:search].downcase
+      @armors = filtered_armors.where("LOWER(name) LIKE ?", "%#{search_term}%").page(params[:page]).per(25)
+    else
+      @armors = filtered_armors.page(params[:page]).per(25)
+    end
   end
 
   def show
-    @armor = Armor.find(params[:id])
     @armor = Armor.includes(armor_crafting_materials: :crafting_material).find(params[:id])
   end
-
 end
--- app/controllers/weapons_controller.rb
+++ app/controllers/weapons_controller.rb
@@ -1,6 +1,10 @@
 class WeaponsController < ApplicationController
   def index
-    @weapons = Weapon.page(params[:page]).per(25) # Adjust if need be
+    if params[:search].present?
+      @weapons = Weapon.where("name LIKE ?", "%#{params[:search]}%").page(params[:page]).per(25) # Adjust if need be
+    else
+      @weapons = Weapon.page(params[:page]).per(25)
+    end
   end
 
   def show
